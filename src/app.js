ba115ea899211b848f1891cc11eae1647ff4bbe7
ruiange <bombingpig@outlook.com>
2025-04-24 18:17:01 +0800

    feat: 初始化项目结构并配置基本服务
    - 新增 mainRouter 文件，用于后续路由注册
    - 引入 express、cors 等依赖
    - 配置 CORS 中间件以支持跨域请求- 设置服务器端口并启动监听

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
+
+
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import getEnvIp from './utils/ipUtil.js';
+import mainRouter from './routes/mainRouter.js';
 
+/** @type {express.Application} */
 const app = express();
+
+// 启用CORS中间件，允许跨域请求
 app.use(cors());
 
+// 注册主路由
+app.use(mainRouter)
 
+// 设置服务器端口，优先使用环境变量中的PORT，默认为3000
 const port = process.env.PORT || 3000;
+
+// 启动服务器并监听指定端口
 app.listen(port, () => {
   console.log(`服务运行在:http://${getEnvIp()}:${port}`);
 })
