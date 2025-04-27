ed03bf39b10827451f1afe990225b4a07b6963f7
ruiange <bombingpig@outlook.com>
2025-04-27 14:58:20 +0800

    feat:配置 EJS模板引擎并调整视图路径
    - 设置 EJS 为默认模板引擎
    - 添加视图路径配置
    - 优化代码格式

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// 配置EJS模板引擎
+app.set('view engine', 'ejs');
+console.log(path.join(__dirname, 'views/pages'))
 app.set('views', path.join(__dirname, 'views/pages'));
-
 // 启用CORS中间件，允许跨域请求
 app.use(
   cors({
@@ -23,7 +25,7 @@ app.use(
 // 解析JSON格式的请求体
 app.use(express.json());
 // 注册主路由
-app.use(mainRouter)
+app.use(mainRouter);
 
 // 设置服务器端口，优先使用环境变量中的PORT，默认为3000
 const port = process.env.PORT || 3000;
@@ -32,4 +34,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`服务运行在:http://${getEnvIp()}:${port}`);
   console.log(`服务运行在:http://${getEnvIp()}:${port}/apidoc`);
-})
+});
