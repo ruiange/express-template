5463bb9e50313835f09b90f7953445aa7f898cca
ruiange <bombingpig@outlook.com>
2025-04-27 11:24:01 +0800

    feat(nineYin): 实现九阴真经新闻爬虫功能
    - 添加 JSDOM 导入，用于解析 HTML
    -优化 scanNews 函数，实现新闻列表爬取
    - 新增新闻项的数据提取逻辑
    - 更新 .gitignore，移除 apidoc 相关配置
    - 在 package.json 中添加 jsdom 依赖

--- src/controllers/nineYinControllers.js
+++ src/controllers/nineYinControllers.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { JSDOM } from 'jsdom';
 /**
  * @description 九阴真经新闻爬虫
  * @param req
@@ -9,7 +9,7 @@ import axios from 'axios';
 export const scanNews = async (req, res) => {
   const jiuYinUrl = 'https://9yin.woniu.com/news/sysnotice';
 
-  const { data } = await axios({
+  const { data: html } = await axios({
     method: 'get',
     url: jiuYinUrl,
     headers: {
@@ -17,9 +17,43 @@ export const scanNews = async (req, res) => {
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     },
   });
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  // 找到新闻列表容器
+  const newsList = document.querySelector('.newsListModCont');
+
+  // 遍历里面的每一条新闻项
+  const newsItems = newsList.querySelectorAll('.newsListItem');
+
+  console.log(newsItems);
+
+  const result = [];
+
+  newsItems.forEach((item) => {
+    const link = item.querySelector('a');
+    const time = item.querySelector('.newsListItemTime');
+
+    const url = link?.href || '';
+    let id = '';
+
+    // 提取 URL 最后数字
+    const match = url.match(/\/(\d+)\.html$/);
+    if (match) {
+      id = match[1]; // 提取到的数字作为唯一标识
+    }
+
+    result.push({
+      newsId: id, // 唯一标识
+      title: link?.textContent.trim() || '',
+      url,
+      time: time?.textContent.trim() || '',
+    });
+  });
+
   res.send({
     code: 2000,
-    data: '',
+    data: result,
     msg: 'success',
   });
 };
