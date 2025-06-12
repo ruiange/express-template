659584f538e3020939bd372d2ca0765c5e627e78
ruiange <bombingpig@outlook.com>
2025-06-12 14:43:57 +0800

    refactor(douyin):优化 Douyin 控制器的逻辑
    - 将 douyinService 函数的第二个参数从 false 修改为 true，以启用新的解析模式
    - 更新成功消息从 'success' 改为 '解析成功'，以更好地反映操作结果

--- src/controllers/douyin.controller.js
+++ src/controllers/douyin.controller.js
@@ -8,10 +8,10 @@ export const analysisVideo = async (req, res) => {
       msg: 'url is required',
     });
   }
-  const data = await douyinService(url, false);
+  const data = await douyinService(url, true);
   res.send({
     code: 2000,
-    msg: 'success',
+    msg: '解析成功',
     data: data,
   });
 };
