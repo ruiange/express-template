17e8c4ed9a1ec6fc8276ca5b147119f93fc79e8b
ruiange <bombingpig@outlook.com>
2025-06-27 16:24:07 +0800

    feat(apidoc): 更新 API 文档并添加新接口
    - 更新了 API 文档中的现有接口信息
    - 新增了抖音相关的三个接口:
      - 保存视频数据接口 (/api/douyin/save)
      - 获取视频数据接口 (/api/douyin/get) - 混合解析单一视频接口 (/api/douyin)- 更新了 API 项目的生成时间和版本信息
    - 优化了多语言文件的格式

--- src/public/apidoc/api_data.js
+++ src/public/apidoc/api_data.js
@@ -1,117 +1,133 @@
-define({
-  api: [
-    {
-      success: {
-        fields: {
-          'Success 200': [
-            {
-              group: 'Success 200',
-              optional: false,
-              field: 'varname1',
-              description: '<p>No type.</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'varname2',
-              description: '<p>With type.</p>',
-            },
-          ],
-        },
-      },
-      type: '',
-      url: '',
-      version: '0.0.0',
-      filename: 'src/public/apidoc/main.js',
-      group: 'C:\\Users\\bombi\\WebstormProjects\\express-template\\src\\public\\apidoc\\main.js',
-      groupTitle:
-        'C:\\Users\\bombi\\WebstormProjects\\express-template\\src\\public\\apidoc\\main.js',
-      name: '',
-    },
-    {
-      type: 'get',
-      url: '/api/9yin/scan/news',
-      title: '获取九阴新闻列表',
-      name: 'GetNineYinNews',
-      group: '九阴',
-      version: '1.0.0',
-      description: '<p>从九阴官网爬取最新的新闻列表信息</p>',
-      success: {
-        fields: {
-          'Success 200': [
-            {
-              group: 'Success 200',
-              type: 'Number',
-              optional: false,
-              field: 'code',
-              description: '<p>状态码 (2000: 成功)</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'msg',
-              description: '<p>响应消息</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'Object[]',
-              optional: false,
-              field: 'data',
-              description: '<p>新闻列表数据</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'data.newsId',
-              description: '<p>新闻ID</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'data.title',
-              description: '<p>新闻标题</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'data.url',
-              description: '<p>新闻链接</p>',
-            },
-            {
-              group: 'Success 200',
-              type: 'String',
-              optional: false,
-              field: 'data.time',
-              description: '<p>发布时间</p>',
-            },
-          ],
-        },
-        examples: [
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
           {
-            title: '成功响应示例:',
-            content:
-              'HTTP/1.1 200 OK\n{\n  "code": 2000,\n  "msg": "success",\n  "data": [{\n    "newsId": "123456",\n    "title": "九阴真经重要更新公告",\n    "url": "https://9yin.woniu.com/news/123456.html",\n    "time": "2024-01-20"\n  }]\n}',
-            type: 'json',
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
           },
-        ],
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "src/public/apidoc/main.js",
+    "group": "C:\\Users\\bombi\\WebstormProjects\\express-template\\src\\public\\apidoc\\main.js",
+    "groupTitle": "C:\\Users\\bombi\\WebstormProjects\\express-template\\src\\public\\apidoc\\main.js",
+    "name": ""
+  },
+  {
+    "type": "get",
+    "url": "/api/9yin/scan/news",
+    "title": "获取九阴新闻列表",
+    "name": "GetNineYinNews",
+    "group": "九阴",
+    "version": "1.0.0",
+    "description": "<p>从九阴官网爬取最新的新闻列表信息</p>",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "code",
+            "description": "<p>状态码 (2000: 成功)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "msg",
+            "description": "<p>响应消息</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "data",
+            "description": "<p>新闻列表数据</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.newsId",
+            "description": "<p>新闻ID</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.title",
+            "description": "<p>新闻标题</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.url",
+            "description": "<p>新闻链接</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.time",
+            "description": "<p>发布时间</p>"
+          }
+        ]
       },
-      filename: 'src/routes/modules/nineYin.router.js',
-      groupTitle: '九阴',
+      "examples": [
+        {
+          "title": "成功响应示例:",
+          "content": "HTTP/1.1 200 OK\n{\n  \"code\": 2000,\n  \"msg\": \"success\",\n  \"data\": [{\n    \"newsId\": \"123456\",\n    \"title\": \"九阴真经重要更新公告\",\n    \"url\": \"https://9yin.woniu.com/news/123456.html\",\n    \"time\": \"2024-01-20\"\n  }]\n}",
+          "type": "json"
+        }
+      ]
     },
-    {
-      type: 'post',
-      url: '/api/douyin',
-      title: '混合解析单一视频接口',
-      name: '混合解析单一视频接口',
-      group: '抖音',
-      version: '1.0.0',
-      filename: 'src/routes/modules/douyin.router.js',
-      groupTitle: '抖音',
-    },
-  ],
-});
+    "filename": "src/routes/modules/nineYin.router.js",
+    "groupTitle": "九阴"
+  },
+  {
+    "type": "get",
+    "url": "/api/douyin/save",
+    "title": "保存视频数据接口",
+    "name": "保存视频数据接口",
+    "group": "抖音",
+    "version": "1.0.0",
+    "filename": "src/routes/modules/douyin.router.js",
+    "groupTitle": "抖音"
+  },
+  {
+    "type": "post",
+    "url": "/api/douyin",
+    "title": "混合解析单一视频接口",
+    "name": "混合解析单一视频接口",
+    "group": "抖音",
+    "version": "1.0.0",
+    "filename": "src/routes/modules/douyin.router.js",
+    "groupTitle": "抖音"
+  },
+  {
+    "type": "get",
+    "url": "/api/douyin/get",
+    "title": "获取视频数据接口",
+    "name": "获取视频数据接口",
+    "group": "抖音",
+    "version": "1.0.0",
+    "filename": "src/routes/modules/douyin.router.js",
+    "groupTitle": "抖音"
+  }
+] });
