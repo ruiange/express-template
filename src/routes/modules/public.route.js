65a18fd5562036437854d79497f4e3dc033f69a5
ruiange <ruiange@aliyun.com>
2025-09-01 21:36:29 +0800

    feat(weather): 添加 ip 打印日志- 在获取天气信息的路由中添加了 ip 打印日志，方便调试和监控-优化了代码格式，增加了可读性

--- src/routes/modules/public.route.js
+++ src/routes/modules/public.route.js
@@ -6,6 +6,8 @@ const publicRoute = express.Router();
 publicRoute.get('/weather', async (req, res) => {
   const clientIP = req.clientIP || 'Unknown';
   let ip = clientIP.replace(/[^0-9.]/g, '');
+
+  console.log(ip, 'ip');
   if (ip === '1' || !ip) ip = '127.0.0.1';
 
   let city = ip;
@@ -13,6 +15,7 @@ publicRoute.get('/weather', async (req, res) => {
   if (city === '127.0.0.1') {
     city = '北京';
   }
+
   const { data } = await axios({
     method: 'get',
     url: 'https://apis.tianapi.com/tianqi/index',
